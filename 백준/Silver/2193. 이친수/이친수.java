c63c7145212af13758604f819adfcf33d1c2ba3a
tkamo2006 <101330766+tkamo2006@users.noreply.github.com>
2024-08-06 17:50:25 +0900

    [Silver III] Title: 이친수, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        long[] dp = new long[91];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        System.out.println(dp[n-1]);
+
+    }
+}
