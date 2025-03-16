7ece70d4ee6f8eb1cda6bc27b71c3a69fada2034
tkamo2006 <101330766+tkamo2006@users.noreply.github.com>
2025-03-17 00:11:55 +0900

    [Silver I] Title: 곱셈, Time: 104 ms, Memory: 14352 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -0,0 +1,31 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        System.out.println(pow(a, b, c));
+
+    }
+
+    static long pow(int a, int b, int mod) {
+        if (b == 0) {
+            return 1;
+        }
+
+        long n = pow(a, b / 2, mod);
+        if (b % 2 == 0) {
+            return n * n % mod;
+        } else {
+            return (n * n % mod) * a % mod;
+        }
+    }
+}
